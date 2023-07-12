069a79f100ba3e2b8adac2019b39d94f22ab8490
Alice Smith <alice@smith.com>
2023-07-12 13:43:23 -0400

    Set up server,js

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+var dao = require("./actions/api");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// server start-up
+const port = 5000
+console.log("Open a browser to http://localhost:"+port+" to view the application");
+app.listen(port);
+
+app.get("/products", (req, res) => {
+    dao.findAllProducts(
+        (products) => {
+            if (!products) {
+                res.status(404).end();
+            } else {
+                res.send(products);
+            }
+        })
+});
+
+app.get("/products/:id", (req, res) => {
+    let id = req.params.id;
+    dao.findProduct(id,
+        (product) => {
+            console.log(product);
+            if (!product) {
+                res.status(404).end();
+            } else {
+                res.send(product);
+            }
+        })
+});
