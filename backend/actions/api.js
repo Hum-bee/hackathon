63c7a7f330f21440bd8d46c41cacb4d59f44120f
Erick Ruilova <erick.123ruilova@gmail.com>
2023-07-13 14:16:38 -0400

    Added homepage contents

--- backend/actions/api.js
+++ backend/actions/api.js
@@ -1,5 +1,5 @@
-const mongodb = require("mongodb"); // mongo client library  
-const { MongoClient } = require('mongodb');
+const mongodb = require("mongodb"); // mongo client library
+const { MongoClient } = require("mongodb");
 
 const url = "mongodb://localhost:27017";
 const dbName = "onlinestore";
@@ -7,30 +7,38 @@ const dbName = "onlinestore";
 let db;
 
 async function startup() {
-    try{
+  try {
     let client = new MongoClient(url);
     await client.connect();
     db = client.db(dbName);
-    console.log('Connected to database');
-    }
-    catch(err){
-        console.log(err);
-    }
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err);
+  }
 }
 startup();
 
 module.exports = {
-    // retrieve all products
-    findAllProducts: function(callback) {
-        let collection = db.collection("products");
-        let dataPromise = collection.find({}).toArray();
-        dataPromise.then((products) => callback(products));
-    },
+  // retrieve all products
+  findAllProducts: function (callback) {
+    let collection = db.collection("products");
+    let dataPromise = collection.find({}).toArray();
+    dataPromise.then((products) => callback(products));
+  },
 
-    // retrieve one film
-    findProduct: function(id, callback) {
-        let collection = db.collection("products");
-        let dataPromise = collection.findOne({"PRODUCT_ID":+id});
-        dataPromise.then((product) => callback(product));
-    }
+  // retrieve one product
+  findProduct: function (id, callback) {
+    let collection = db.collection("products");
+    let dataPromise = collection.findOne({ PRODUCT_ID: +id });
+    dataPromise.then((product) => callback(product));
+  },
+
+  // retrieve one product
+  findProductRandom: function (callback) {
+    let collection = db.collection("products");
+    let dataPromise = collection
+      .aggregate([{ $sample: { size: 10 } }])
+      .toArray();
+    dataPromise.then((product) => callback(product));
+  },
 };
--- backend/server.js
+++ backend/server.js
@@ -9,30 +9,42 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // server start-up
-const port = 5000
-console.log("Open a browser to http://localhost:"+port+" to view the application");
+const port = 5000;
+console.log(
+  "Open a browser to http://localhost:" + port + " to view the application"
+);
 app.listen(port);
 
 app.get("/products", (req, res) => {
-    dao.findAllProducts(
-        (products) => {
-            if (!products) {
-                res.status(404).end();
-            } else {
-                res.send(products);
-            }
-        })
+  dao.findAllProducts((products) => {
+    if (!products) {
+      res.status(404).end();
+    } else {
+      res.send(products);
+    }
+  });
 });
 
 app.get("/products/:id", (req, res) => {
-    let id = req.params.id;
-    dao.findProduct(id,
-        (product) => {
-            //console.log(product);
-            if (!product) {
-                res.status(404).end();
-            } else {
-                res.send(product);
-            }
-        })
+  let id = req.params.id;
+  dao.findProduct(id, (product) => {
+    //console.log(product);
+    if (!product) {
+      res.status(404).end();
+    } else {
+      res.send(product);
+    }
+  });
+});
+
+app.get("/randomProducts", (req, res) => {
+  console.log("test");
+  dao.findProductRandom((products) => {
+    console.log(products);
+    if (!products) {
+      res.status(404).end();
+    } else {
+      res.send(products);
+    }
+  });
 });
