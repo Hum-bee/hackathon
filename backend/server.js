6f736f2820aed032eab16e02988c61bd567ad605
Alice Smith <alice@smith.com>
2023-07-13 12:11:25 -0400

    Implement products API search and filter functionality.

--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,23 @@ console.log("Open a browser to http://localhost:"+port+" to view the application
 app.listen(port);
 
 app.get("/products", (req, res) => {
-    dao.findAllProducts(
-        (products) => {
-            if (!products) {
-                res.status(404).end();
-            } else {
-                res.send(products);
-            }
-        })
-});
-
+    
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 4;
+    const offset = (page - 1) * limit;
+    const searchTerm = req.query.searchTerm || "";
+    const filter = req.query.filter || "";
+    
+    dao.findAllProducts(searchTerm, filter, (products) => {
+      if (!products) {
+        res.status(404).end();
+      } else {
+        const paginatedProducts = products.slice(offset, offset + limit);
+        res.send(paginatedProducts);
+      }
+    })
+  });
+  
 app.get("/products/:id", (req, res) => {
     let id = req.params.id;
     dao.findProduct(id,
