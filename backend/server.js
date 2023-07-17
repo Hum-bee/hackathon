e008285115854d500d05a9e6307f85851c3a327b
Hum-bee <hsheredia1@gmail.com>
2023-07-17 02:45:36 -0400

    Improve error handling for findProduct.

--- backend/server.js
+++ backend/server.js
@@ -33,17 +33,19 @@ app.get("/products", (req, res) => {
 });
 
 app.get("/products/:id", (req, res) => {
-  let id = req.params.id;
-  dao.findProduct(id, (product) => {
-    //console.log(product);
-    if (!product) {
-      res.status(404).end();
-    } else {
-      res.send(product);
-    }
+    let id = req.params.id;
+    dao.findProduct(id, (product) => {
+      if (product instanceof Error) {
+        console.error(product);
+        res.status(500).end();
+      } else if (!product) {
+        res.status(404).end();
+      } else {
+        res.send(product);
+      }
+    });
   });
-});
-
+  
 app.get("/randomProducts", (req, res) => {
   dao.findProductRandom((products) => {
     if (!products) {
